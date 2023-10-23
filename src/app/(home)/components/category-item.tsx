4f47c3380ec476ca11a80f2383a921b92f7e0db6
Leonardo Marconi <leonardo.marconi.pf@hotmail.com>
2023-10-23 17:12:08 -0300

    feat: ajust badge component

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -17,7 +17,7 @@ const CategoryItem = ({category}: CategoryItemProps) => {
     }
 
     return (
-        <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
+        <Badge variant="secondary" className="py-3 flex justify-center items-center gap-2 rounded-lg">
             {categoryIcon[category.slug as keyof typeof categoryIcon]}
             <span className="font-bold text-xs">{category.name}</span>
         </Badge>
