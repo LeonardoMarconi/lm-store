4f48c75d1c7c8e91449138a053cc65e69e2d5523
Leonardo Marconi <leonardo.marconi.pf@hotmail.com>
2023-10-23 17:12:37 -0300

    feat: add Banner component

---
+++ src/components/ui/banner.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+const Banner = () => {
+    return ( 
+        <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image 
+          src="/banner_home_01.png"
+          height={0}
+          width={0}
+          className='h-auto w-full'
+          sizes='100vw'
+          alt='Periféricos a partir de R$ 99,00'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image 
+            src="/banner_home_02.png"
+            height={0}
+            width={0}
+            className='h-auto w-full'
+            sizes='100vw'
+            alt='Periféricos a partir de R$ 99,00'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image 
+            src="/banner_home_03.png"
+            height={0}
+            width={0}
+            className='h-auto w-full'
+            sizes='100vw'
+            alt='Periféricos a partir de R$ 99,00'
+          />
+        </SwiperSlide>
+      </Swiper>
+     );
+}
+ 
+export default Banner;
